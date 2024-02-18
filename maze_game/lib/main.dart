4abf90e417a5f3fe9a19b7f5b9be3cce28d12258
abdyevas <sabina.abdyeva@ufaz.az>
2024-02-18 22:41:27 +0400

    now it is about mazesweeper

--- maze_game/lib/main.dart
+++ maze_game/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:maze_game/maze_game.dart';
+import 'package:maze_game/minesweeper.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MazeGame(),
+    return const MaterialApp(
+      home: Minesweeper(),
     );
   }
 }
---
+++ maze_game/lib/minesweeper.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class Minesweeper extends StatefulWidget {
+  const Minesweeper({super.key});
+
+  @override
+  State<Minesweeper> createState() => _MinesweeperState();
+}
+
+class _MinesweeperState extends State<Minesweeper> {
+  List<List<bool>> board = [];
+
+  @override
+  void initState() {
+    super.initState();
+    initializeBoard();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Minesweeper Game'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(
+            board.length,
+            (rowIndex) {
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(
+                  board[rowIndex].length,
+                  (colIndex) {
+                    return GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        width: 30,
+                        height: 30,
+                        margin: const EdgeInsets.all(1),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: const Color.fromARGB(255, 54, 54, 54)),
+                          color: board[rowIndex][colIndex]
+                              ? Colors.grey 
+                              : Colors.white, 
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  void initializeBoard() {
+    for (int i = 0; i < 10; i++) {
+      board.add(List.generate(10, (j) => true));
+    }
+  }
+}
