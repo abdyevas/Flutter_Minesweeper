dd40859e21ab2c1e124a973eac32784013405fc6
abdyevas <sabina.abdyeva@ufaz.az>
2024-03-16 23:55:53 +0400

    edits styling

--- minesweeper/lib/screens/minesweeper.dart
+++ minesweeper/lib/screens/minesweeper.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
+import 'package:maze_game/widgets/create_board.dart';
 
 class Minesweeper extends StatefulWidget {
   const Minesweeper({super.key});
@@ -17,8 +17,8 @@ class _MinesweeperState extends State<Minesweeper> {
   void initState() {
     super.initState();
     initializeBoard();
-    placeMines();
-    calculateNeighbors();
+    placeMines(board, numMines);
+    calculateNeighbors(board);
   }
 
   @override
@@ -50,22 +50,24 @@ class _MinesweeperState extends State<Minesweeper> {
                           border: Border.all(
                               color: const Color.fromARGB(255, 54, 54, 54)),
                           color: revealed[rowIndex][colIndex]
-                              ? const Color.fromARGB(255, 8, 29, 58)
+                              ? board[rowIndex][colIndex] == -1
+                                  ? Colors.red
+                                  : const Color.fromARGB(255, 8, 29, 58)
                               : const Color.fromARGB(255, 78, 110, 167),
                         ),
                         child: Center(
                           child: board[rowIndex][colIndex] > -1
-                          ? Text(
-                            board[rowIndex][colIndex].toString(),
-                            style: const TextStyle(
-                              color: Colors.white,
-                            ),
-                          )
-                          : Image.asset(
-                            'assets/images/mine_icon.png',
-                            width: 20,
-                            height: 20,
-                          ),
+                              ? Text(
+                                  board[rowIndex][colIndex].toString(),
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                )
+                              : Image.asset(
+                                  'assets/images/mine_icon.png',
+                                  width: 20,
+                                  height: 20,
+                                ),
                         ),
                       ),
                     );
@@ -79,57 +81,9 @@ class _MinesweeperState extends State<Minesweeper> {
     );
   }
 
-  void initializeBoard() {
-    for (int i = 0; i < 10; i++) {
-      board.add(List.generate(10, (j) => 0));
-      revealed.add(List.generate(10, (j) => false));
-    }
-  }
-
-  void placeMines() {
-    int minesPlaced = 0;
-    Random random = Random();
-
-    while (minesPlaced < numMines) {
-      int i = random.nextInt(10);
-      int j = random.nextInt(10);
-
-      if (board[i][j] == 0) {
-        board[i][j] = -1;
-        minesPlaced++;
-      }
-    }
-  }
-
-  void calculateNeighbors() {
-    for (int i = 0; i < 10; i++) {
-      for (int j = 0; j < 10; j++) {
-        if (board[i][j] == 0) {
-          int minesCount = 0;
-
-          for (int x = -1; x <= 1; x++) {
-            for (int y = -1; y <= 1; y++) {
-              int neighborRow = i + x;
-              int neighborCol = j + y;
-
-              if (neighborRow >= 0 &&
-                  neighborRow < 10 &&
-                  neighborCol >= 0 &&
-                  neighborCol < 10 &&
-                  board[neighborRow][neighborCol] == -1) {
-                minesCount++;
-              }
-            }
-          }
-          board[i][j] = minesCount;
-        }
-      }
-    }
-  }
-
   void revealCell(int rowIndex, int colIndex) {
     setState(() {
-      revealed[rowIndex][colIndex] = true;      
+      revealed[rowIndex][colIndex] = true;
     });
   }
 }
