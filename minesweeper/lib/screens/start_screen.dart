4653239362240fb0cafd3a04776e6031b7b32d7a
abdyevas <sabina.abdyeva@ufaz.az>
2024-03-27 23:49:33 +0400

    edits

--- minesweeper/lib/screens/start_screen.dart
+++ minesweeper/lib/screens/start_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatefulWidget {
-  const StartScreen({super.key});//, required this.switchScreen});
+  const StartScreen({super.key}); //, required this.switchScreen});
 
   // final void Function(bool isOnePlayer) switchScreen;
 
@@ -19,17 +19,23 @@ class _StartScreenState extends State<StartScreen> {
           const SizedBox(
             height: 20,
           ),
-          const Text('Welcome to Minesweeper'),
+          const Text(
+            'Welcome to Minesweeper',
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.amber,
+            ),
+          ),
           const SizedBox(
             height: 30,
           ),
           ElevatedButton(
             onPressed: () {},
-            child: const Text('Easy'), // 8 x 8 - 10 
+            child: const Text('Easy'), // 8 x 8 - 10
           ),
           ElevatedButton(
             onPressed: () {},
-            child: const Text('Standart'), // 16 x 16 - 40 
+            child: const Text('Standart'), // 16 x 16 - 40
           ),
           ElevatedButton(
             onPressed: () {},
