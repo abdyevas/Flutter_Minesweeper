b6c16014388f9ddbd6fd83985707c58f4d26d5ed
abdyevas <sabina.abdyeva@ufaz.az>
2024-05-31 23:42:47 +0400

    some upds

--- minesweeper/lib/main.dart
+++ minesweeper/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:maze_game/screens/minesweeper.dart';
-import 'package:maze_game/screens/start_screen.dart';
+// import 'package:maze_game/screens/start_screen.dart';
 
 void main() {
   runApp(const MyApp());
